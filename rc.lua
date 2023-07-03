7d5f1475eecce52fd1c605fa98dd254d4359eb59
Andreas Petersen <andreasgdp@gmail.com>
2023-07-03 18:59:25 +0200

    Change to rofi app launcher

--- rc.lua
+++ rc.lua
@@ -469,9 +469,12 @@ globalkeys = gears.table.join(
 		})
 	end, { description = "lua execute prompt", group = "awesome" }),
 	-- Menubar
+	-- awful.key({ modkey }, "p", function()
+	-- 	menubar.show()
+	-- end, { description = "show the menubar", group = "launcher" }),
 	awful.key({ modkey }, "p", function()
-		menubar.show()
-	end, { description = "show the menubar", group = "launcher" })
+		awful.spawn("rofi -show drun")
+	end, { description = "rofi launcher", group = "launcher" })
 )
 
 clientkeys = gears.table.join(
