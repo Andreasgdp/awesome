699364f8fdf9708863e5b65c3a3872c162bec38b
Andreas Petersen <andreasgdp@gmail.com>
2023-07-18 09:00:32 +0200

    Fix issues where clients overlap with wibar
    Whan resizing with gaps some windows will og out of bounds
    and go over the wibar, if there is not enough room.
    This addresses that.

--- rc.lua
+++ rc.lua
@@ -627,6 +627,7 @@ awful.rules.rules = {
 			buttons = clientbuttons,
 			screen = awful.screen.preferred,
 			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+			size_hints_honor = false
 		},
 	},
 
