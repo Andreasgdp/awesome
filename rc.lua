32255506cc60daf454bdbaecacd6f9740a346313
Andreas Petersen <andreasgdp@gmail.com>
2024-08-30 18:21:50 +0200

    feat: improve client (window) navigation
    cursor moves to focused window
    manager attempts to always focus window under cursor
    enable relative navigation with hjkl

--- rc.lua
+++ rc.lua
@@ -45,6 +45,17 @@ if awesome.startup_errors then
 	})
 end
 
+local function move_mouse_onto_focused_client(c)
+	client.focus = c
+	c:raise()
+	if mouse.object_under_pointer() ~= c then
+		local geometry = c:geometry()
+		local x = geometry.x + geometry.width / 2
+		local y = geometry.y + geometry.height / 2
+		mouse.coords({ x = x, y = y }, true)
+	end
+end
+
 -- Handle runtime errors after startup
 do
 	local in_error = false
@@ -86,21 +97,7 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.tile.left,
 	awful.layout.suit.tile,
-	awful.layout.suit.tile.bottom,
-	awful.layout.suit.tile.top,
-	awful.layout.suit.fair,
-	awful.layout.suit.fair.horizontal,
-	awful.layout.suit.spiral,
-	awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
-	awful.layout.suit.max.fullscreen,
-	awful.layout.suit.magnifier,
-	awful.layout.suit.corner.nw,
-	awful.layout.suit.floating, -- awful.layout.suit.corner.ne,
-	-- awful.layout.suit.corner.sw,
-	-- awful.layout.suit.corner.se,
 }
 -- }}}
 
@@ -316,12 +313,10 @@ root.buttons(gears.table.join(
 globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 	awful.key({}, "Print", function()
 		awful.spawn("flameshot gui")
-	end), -- sudo add-apt-repository ppa:peek-developers/stable
-	-- sudo apt-get update
-	-- sudo apt-get install peek
+	end),
 	awful.key({ modkey }, "Print", function()
 		awful.spawn("peek")
-	end), -- Configure the hotkeys for volume
+	end),
 	awful.key({}, "#123", function()
 		volume_widget:inc(5)
 	end),
@@ -330,8 +325,7 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 	end),
 	awful.key({}, "#121", function()
 		volume_widget:toggle()
-	end), -- Configure the hotkeys for media control
-	-- configure brightness control
+	end),
 	awful.key({}, "XF86MonBrightnessUp", function()
 		brightness_widget:inc()
 	end),
@@ -379,12 +373,7 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 	end, {
 		description = "lock Screen",
 		group = "awesome",
-	}), -- Configure keyboardlayout switcher using setxkbmap us and setxkbmap dk
-	-- awful.key({ modkey }, "d", function()
-	-- 	awful.spawn("setxkbmap dk")
-	-- end, {
-	-- 	description = "switch keyboardlayout to Danish DK",
-	-- }),
+	}),
 	awful.key({ modkey }, "e", function()
 		awful.spawn("setxkbmap us altgr-intl")
 	end, {
@@ -483,14 +472,6 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 		description = "open obsidian",
 		group = "launcher",
 	}), -- Open awesome config in Code - Insiders with super + a
-	awful.key({ modkey }, "a", function()
-		-- awful.spawn.with_shell("terminator -e 'cd ~/.config/awesome/; nvim .'")
-		awful.spawn.with_shell("kitty sh -c 'cd ~/.config/awesome/; nvim .'")
-	end, {
-		description = "open awesome config",
-		group = "launcher",
-	}), -- Using alt + space run the akiflow-command-bar.sh script
-	-- Make sure to have xdotool istalled - sudo apt install xdotool
 	awful.key({ "Mod1" }, "space", function()
 		awful.util.spawn("bash -c  '~/.config/awesome/launch-files/akiflow-command-bar.sh'")
 	end, {
@@ -510,144 +491,26 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 		description = "view next",
 		group = "tag",
 	}),
-	awful.key({ modkey, "Mod1" }, "Right", function()
-		-- move selected window in current tag to the right tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local next_tag_index = curr_tag_index + 1
-		local next_tag = curr_screen.tags[next_tag_index]
-		if next_tag then
-			local c = client.focus
-			if c then
-				c:move_to_tag(next_tag)
-			end
-		end
-		awful.tag.viewnext()
-	end, {
-		description = "move selected window in current tag to the right tag",
-		group = "screen",
-	}),
-	awful.key({ modkey, "Mod1" }, "Left", function()
-		-- move selected window in current tag to the left tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local prev_tag_index = curr_tag_index - 1
-		local prev_tag = curr_screen.tags[prev_tag_index]
-		if prev_tag then
-			local c = client.focus
-			if c then
-				c:move_to_tag(prev_tag)
-			end
-		end
-		awful.tag.viewprev()
-	end, {
-		description = "move selected window in current tag to the left tag",
-		group = "screen",
-	}),
-	awful.key({ modkey, "Control" }, "h", awful.tag.viewprev, {
-		description = "view previous",
-		group = "tag",
-	}),
-	awful.key({ modkey, "Control" }, "l", awful.tag.viewnext, {
-		description = "view next",
-		group = "tag",
-	}),
-	awful.key({ modkey, "Control", "Mod1" }, "l", function()
-		-- move selected window in current tag to the right tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local next_tag_index = curr_tag_index + 1
-		local next_tag = curr_screen.tags[next_tag_index]
-		if next_tag then
-			local c = client.focus
-			if c then
-				c:move_to_tag(next_tag)
-			end
-		end
-		awful.tag.viewnext()
-	end, {
-		description = "move selected window in current tag to the right tag",
-		group = "screen",
-	}),
-	awful.key({ modkey, "Control", "Mod1" }, "h", function()
-		-- move selected window in current tag to the left tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local prev_tag_index = curr_tag_index - 1
-		local prev_tag = curr_screen.tags[prev_tag_index]
-		if prev_tag then
-			local c = client.focus
-			if c then
-				c:move_to_tag(prev_tag)
-			end
-		end
-		awful.tag.viewprev()
-	end, {
-		description = "move selected window in current tag to the left tag",
-		group = "screen",
-	}),
-	awful.key({ modkey, "Control", "Mod1" }, "Right", function()
-		-- move all windows in current tag to the right tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local next_tag_index = curr_tag_index + 1
-		local next_tag = curr_screen.tags[next_tag_index]
-		if next_tag then
-			for _, c in ipairs(curr_tag:clients()) do
-				c:move_to_tag(next_tag)
-			end
-		end
-		awful.tag.viewnext()
-	end, {
-		description = "move all windows in current tag to the right tag",
-		group = "screen",
-	}),
-	awful.key({ modkey, "Control", "Mod1" }, "Left", function()
-		-- move all windows in current tag to the left tag
-		local curr_screen = awful.screen.focused()
-		local curr_tag = curr_screen.selected_tag
-		local curr_tag_index = curr_tag.index
-		local prev_tag_index = curr_tag_index - 1
-		local prev_tag = curr_screen.tags[prev_tag_index]
-		if prev_tag then
-			for _, c in ipairs(curr_tag:clients()) do
-				c:move_to_tag(prev_tag)
-			end
-		end
-		awful.tag.viewprev()
-	end, {
-		description = "move all windows in current tag to the right tag",
-		group = "screen",
-	}),
-	-- awful.key({ modkey }, "Escape", awful.tag.history.restore, {
-	-- 	description = "go back",
-	-- 	group = "tag",
-	-- }),
 	awful.key({ modkey }, "j", function()
-		awful.client.focus.byidx(1)
-	end, {
-		description = "focus next by index",
-		group = "client",
-	}),
-	awful.key({ modkey }, "Down", function()
-		awful.client.focus.byidx(1)
+		awful.client.focus.global_bydirection("down")
 	end, {
 		description = "focus next by index",
 		group = "client",
 	}),
 	awful.key({ modkey }, "k", function()
-		awful.client.focus.byidx(-1)
+		awful.client.focus.global_bydirection("up")
 	end, {
 		description = "focus previous by index",
 		group = "client",
 	}),
-	awful.key({ modkey }, "Up", function()
-		awful.client.focus.byidx(-1)
+	awful.key({ modkey }, "h", function()
+		awful.client.focus.global_bydirection("left")
+	end, {
+		description = "focus previous by index",
+		group = "client",
+	}),
+	awful.key({ modkey }, "l", function()
+		awful.client.focus.global_bydirection("right")
 	end, {
 		description = "focus previous by index",
 		group = "client",
@@ -658,28 +521,33 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 		description = "show main menu",
 		group = "awesome",
 	}), -- Layout manipulation
+	-- hjkl swap by direction
 	awful.key({ modkey, "Shift" }, "j", function()
-		awful.client.swap.byidx(1)
+		awful.client.swap.global_bydirection("down")
+		awful.client.focus.global_bydirection("down")
 	end, {
-		description = "swap with next client by index",
-		group = "client",
-	}),
-	awful.key({ modkey, "Shift" }, "Down", function()
-		awful.client.swap.byidx(1)
-	end, {
-		description = "swap with next client by index",
+		description = "swap with client below",
 		group = "client",
 	}),
 	awful.key({ modkey, "Shift" }, "k", function()
-		awful.client.swap.byidx(-1)
+		awful.client.swap.global_bydirection("up")
+		awful.client.focus.global_bydirection("up")
 	end, {
-		description = "swap with previous client by index",
+		description = "swap with client above",
 		group = "client",
 	}),
-	awful.key({ modkey, "Shift" }, "Up", function()
-		awful.client.swap.byidx(-1)
+	awful.key({ modkey, "Shift" }, "h", function()
+		awful.client.swap.global_bydirection("left")
+		awful.client.focus.global_bydirection("left")
 	end, {
-		description = "swap with previous client by index",
+		description = "swap with client to the left",
+		group = "client",
+	}),
+	awful.key({ modkey, "Shift" }, "l", function()
+		awful.client.swap.global_bydirection("right")
+		awful.client.focus.global_bydirection("right")
+	end, {
+		description = "swap with client to the right",
 		group = "client",
 	}),
 	awful.key({ modkey, "Control" }, "j", function()
@@ -733,66 +601,18 @@ globalkeys = gears.table.join( -- Configure the hotkeys for screenshot
 		description = "quit awesome",
 		group = "awesome",
 	}),
-	awful.key({ modkey }, "l", function()
-		awful.tag.incmwfact(-0.05)
+	awful.key({ modkey, "Control" }, "l", function()
+		awful.tag.incmwfact(0.05)
 	end, {
 		description = "increase master width factor",
 		group = "layout",
 	}),
-	awful.key({ modkey }, "h", function()
-		awful.tag.incmwfact(0.05)
+	awful.key({ modkey, "Control" }, "h", function()
+		awful.tag.incmwfact(-0.05)
 	end, {
 		description = "decrease master width factor",
 		group = "layout",
 	}),
-	awful.key({ modkey, "Shift" }, "h", function()
-		awful.tag.incnmaster(-1, nil, true)
-	end, {
-		description = "increase the number of master clients",
-		group = "layout",
-	}),
-	awful.key({ modkey, "Shift" }, "l", function()
-		awful.tag.incnmaster(1, nil, true)
-	end, {
-		description = "decrease the number of master clients",
-		group = "layout",
-	}),
-	awful.key({ modkey, "Control" }, "h", function()
-		awful.tag.incncol(1, nil, true)
-	end, {
-		description = "increase the number of columns",
-		group = "layout",
-	}),
-	awful.key({ modkey, "Control" }, "l", function()
-		awful.tag.incncol(-1, nil, true)
-	end, {
-		description = "decrease the number of columns",
-		group = "layout",
-	}),
-	awful.key({ modkey }, "space", function()
-		awful.layout.inc(1)
-	end, {
-		description = "select next",
-		group = "layout",
-	}),
-	awful.key({ modkey, "Shift" }, "space", function()
-		awful.layout.inc(-1)
-	end, {
-		description = "select previous",
-		group = "layout",
-	}),
-	awful.key({ modkey, "Control" }, "n", function()
-		local c = awful.client.restore()
-		-- Focus restored client
-		if c then
-			c:emit_signal("request::activate", "key.unminimize", {
-				raise = true,
-			})
-		end
-	end, {
-		description = "restore minimized",
-		group = "client",
-	}), -- Prompt
 	awful.key({ modkey }, "r", function()
 		awful.screen.focused().mypromptbox:run()
 	end, {
@@ -1089,9 +909,7 @@ awful.rules.rules = { -- All clients will match this rule.
 		properties = {
 			titlebars_enabled = false,
 		},
-	}, -- Set Firefox to always map on the tag named "2" on screen 1.
-	-- { rule = { class = "Firefox" },
-	--   properties = { screen = 1, tag = "2" } },
+	},
 }
 -- }}}
 
@@ -1186,9 +1004,28 @@ client.connect_signal("mouse::enter", function(c)
 	})
 end)
 
+-- TODO: find a better way to focus the client under the cursor that does not involve a timer
+local timer = gears.timer({ timeout = 0.1 })
+
+-- In the timer's callback function, check the client under the cursor
+timer:connect_signal("timeout", function()
+	-- Get the client under the cursor
+	local cc = awful.mouse.client_under_pointer()
+
+	-- If there is a client under the cursor and it's not the currently focused client, focus it
+	if cc and client.focus ~= cc then
+		client.focus = cc
+	end
+end)
+
+-- Start the timer
+timer:start()
+
 client.connect_signal("focus", function(c)
 	c.border_color = beautiful.border_focus
+	move_mouse_onto_focused_client(c)
 end)
+
 client.connect_signal("unfocus", function(c)
 	c.border_color = beautiful.border_normal
 end)
