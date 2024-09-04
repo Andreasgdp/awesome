b053227de7c0cabf4f3691bf5b38b773a79aaf7e
Andreas Petersen <andreasgdp@gmail.com>
2024-09-04 19:16:17 +0200

    feat: enable picom for animations

--- autostart.sh
+++ autostart.sh
@@ -11,7 +11,9 @@ if command -v streamdeck >/dev/null 2>&1; then
 	run "streamdeck -n"
 fi
 
+killall picom
 run "setxkbmap us"
+run "picom --backend glx --experimental-backends"
 run "/home/$USER/.screenlayout/defaultDisplaySetup.sh"
 
 # if nordvpn is installed, connect to us server
